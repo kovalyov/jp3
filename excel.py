17a47a1780f78610403364cba342eba7f93e274e
olhrabar <38593768+olhrabar@users.noreply.github.com>
2018-10-10 19:47:19 +0300

    Update from Oleksandr

--- excel.py
+++ excel.py
@@ -1,28 +1,53 @@
-import openpyxl
+import datetime
+from openpyxl import load_workbook
 
-print("Введите название файла :", )
+time = datetime.datetime.now().strftime("_%I_%M_%S_%B_%d_%Y")
+print("Введите название файла :\n", "Пример - C:/Users/ohrabar/PycharmProjects/untitled/first_file.xlsx")
 # file_name = input() + ".xlsx"
-file_name = 'test.xlsx'
+file_name = 'first_file.xlsx'
 
 # Load in the workbook
-wb = openpyxl.load_workbook(file_name)
+wb = load_workbook(file_name)
 
 # Get sheet names
 print(wb.sheetnames)
 
-# sheet = wb.get_sheet_by_name('Sheet3')    #DeprecationWarning: Call to deprecated function get_sheet_by_name (Use wb[sheetname]).
-# sheet = wb.active # Choose last active sheet before closing a file
-sheet = wb['Sheet3']
-print(str(sheet.title))
+sheet = wb['Sheet1']
+print("Работаем во вкладке: ", str(sheet.title))
 
 ch = 'A'
 row = 1
-while chr(ord(ch)) <= "H":      # Limitation a range of columns
-    print("Column Symbol:", ch)
+
+column_1 = 0
+column_2 = 0
+column_3 = 0
+while chr(ord(ch)) <= "Z":  # Limitation a range of columns
+    # print("Column Symbol:", ch)
+
     cell = sheet[str(ch) + str(row)]
-    print("Value: ", cell.value)
+    # print("Value: ", cell.value)
+    if cell.value == "FirstValue":
+        column_1 = cell.column
+    if cell.value == "SecondValue":
+        column_2 = cell.column
+    if cell.value == "Total":
+        column_3 = cell.column
     # print("Row: ",cell.row)
     # print("Column: ",cell.column)
-    print("Coordinate: ", cell.coordinate, "\n==================|")
+    # print("Coordinate: ", cell.coordinate, "\n==================|")
     ch = chr(ord(ch) + 1)
+    # row += 1
+
+print("First: ", str(column_1))
+print("Second: ", str(column_2))
+print("Third: ", str(column_3))
+
+while sheet[str(column_1) + str(row + 1)].value is not None:
+    a = sheet[str(column_1) + str(row + 1)].value
+    b = sheet[str(column_2) + str(row + 1)].value
+    res = a + b
+    sheet[str(column_3) + str(row + 1)] = res
+    print(res)
     row += 1
+
+wb.save("reports/first_file" + time + ".xlsx")
